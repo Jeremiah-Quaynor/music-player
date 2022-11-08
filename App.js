992ab18b31f55b642a1e6153f90ec4194c1ebe81
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-11-08 11:51:33 +0000

    building browser page

--- App.js
+++ App.js
@@ -1,24 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Music player</Text>
-      <Text>Welcome to Music player</Text>
-      <Text>Welcome to Music player</Text>
 
-      <StatusBar style="auto" />
-    </View>
+
+
+
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topbar}>
+        <TouchableOpacity style={styles.back}>
+          <Entypo name="chevron-left" size={30} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.searchbutton}>
+          <Feather name="search" size={20} color="white" />
+        </TouchableOpacity>
+
+      </View>
+      <Text style={styles.browse}>Browse</Text>
+      <ScrollView 
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+
+      </ScrollView>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1,  
+    backgroundColor: '#211649',
+  },
+  topbar: {
+    flexDirection: 'row',
+    height: 40,
+    justifyContent:'space-between',
+    paddingHorizontal: 10
+
+  },
+  searchbutton: {
+    width: 40,
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'white',
+    borderWidth: 2,
+    marginRight: 10
+  },
+  back:{
     alignItems: 'center',
     justifyContent: 'center',
   },
+  browse:{
+    fontSize: 45,
+    fontFamily: 'Helvetica',
+    color: 'white',
+    fontWeight: 'bold',
+    marginVertical: 25,
+    marginLeft: 20
+    
+  }
+
+
 
 });
